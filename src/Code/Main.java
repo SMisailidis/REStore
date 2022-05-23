a4b0bc11f7af5adf7b6ff6873e2cd072d2093801
Savvas Misailidis <savvasmis@hotmail.gr>
2022-05-23 19:59:28 +0300

    temp hum speed

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,33 +1,56 @@
 package Code;
 
-import org.json.simple.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)   {
 		
-		//new LoginPage();
 		HttpRequest request = HttpRequest.newBuilder()
-		        .uri(URI.create("https://community-open-weather-map.p.rapidapi.com/weather?q=Thessaloniki%2Cgr&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml%22"))
+		        .uri(URI.create("https://community-open-weather-map.p.rapidapi.com/weather?q=Thessaloniki%2Cgr&lat=40.6403&lon=22.9439&callback=0&id=2172797&lang=null&units=imperial&mode=xml"))
 		        .header("X-RapidAPI-Host", "community-open-weather-map.p.rapidapi.com")
 		        .header("X-RapidAPI-Key", "bc9d42c52amshf9f60ca66e7b81fp17604fjsn229b971851b5")
 		        .method("GET", HttpRequest.BodyPublishers.noBody())
 		        .build();
 		HttpResponse<String> response;
+		
+		JSONParser parser = new JSONParser();
+		
 		try {
 			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-			System.out.println(response.body());
+
+			String data = response.body();
+						
+			data = data.substring(1, data.length() -1);
+			data = data.substring(1);			
+
+		    JSONObject jsonObject = (JSONObject) JSONValue.parse(data);
+		    
+		    //--Εδω παιρνει την θερμοκρασια και την υγρασια--//
+		    JSONObject takeKeyForTemp = (JSONObject) jsonObject.get("main");
+		    double temperature = (Double) takeKeyForTemp.get("temp");
+		    Long humidity = (Long) takeKeyForTemp.get("humidity");
+		    
+		    
+		    //--Εδω παιρνει τα μποφορ--//
+		    JSONObject takeKeyForWind = (JSONObject) jsonObject.get("wind");
+		    double speed = (Double) takeKeyForWind.get("speed");
+		    
+		    //--to do να παρουμε key:weather, value:"Clouds"--//
 			
-			JSONObject obj = new JSONObject();
 		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
 	}
 
 }
